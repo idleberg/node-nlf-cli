c4a8b584350d161df643e918ee9fc6c3db1c93d8
Jan T. Sott <git@idleberg.com>
2022-02-04 09:54:22 +0100

    rename entry point

--- index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// eslint-disable-next-line
-const resolve = require('path').resolve;
-
-/*  The TypeScript compiler does not support she-bangs,
- *  so we need this stupid workaround 🙄
- */
-require(resolve(__dirname, 'bin/cli.js'));
