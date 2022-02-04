9c6219d0d7605c426f6dd936d0616455c2ffdd7f
Jan T. Sott <git@idleberg.com>
2022-02-04 09:51:55 +0100

    update config

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,20 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
 
-const plugins = [
-  commonjs(),
-  json(),
-  typescript({
-    allowSyntheticDefaultImports: true,
-    strictNullChecks: true
-  })
-];
+const defaults = {
+  external: [ 'crypto' ],
+  output: {
+    file: 'bin/cli.mjs',
+    format: 'esm'
+  },
+  plugins: [
+    commonjs(),
+    json(),
+    typescript()
+  ]
+};
 
-export default [
-  {
-    input: './src/cli.ts',
-    output: {
-      dir: 'bin',
-      format: 'cjs'
-    },
-    plugins: plugins
-  }
-];
+export default {
+  ...defaults,
+  input: 'src/cli.ts'
+};
