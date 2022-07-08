931a84d53b0a5b65e55a8ab9d4ff9e2decbd4628
idleberg <git@idleberg.com>
2022-07-08 18:13:26 +0200

    fix reading manifest

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -1,12 +1,15 @@
-import { basename, extname, join } from 'node:path';
+import { dirname, resolve, basename, extname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { promises } from 'node:fs';
 import * as NLF from '@nsis/nlf';
 import getStdin from 'get-stdin';
 import program from 'commander';
 import symbols from 'log-symbols';
-import { promises } from 'node:fs';
 
 // Dependencies
-const { version } = JSON.parse(await promises.readFile('./package.json', 'utf8'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const { version } = JSON.parse(await promises.readFile(resolve(__dirname, '../package.json'), 'utf8'));
 // Action
 program
     .version(version)
--- index.mjs
+++ index.mjs
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
-import { dirname, join, resolve } from 'node:path';
+import { dirname, resolve } from 'node:path';
 import { promises as fs } from 'node:fs';
 import updateNotifier from 'update-notifier';
 import './bin/cli.mjs';
 
 (async () => {
-  const __dirname = resolve(dirname('')); 
-  const pkg = await fs.readFile(join(__dirname, './package.json'), 'utf8');
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const pkg = await fs.readFile(resolve(__dirname, '../package.json'), 'utf8');
 
   updateNotifier({
     pkg
