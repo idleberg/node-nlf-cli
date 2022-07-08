db125d766fc1292450e5d6fb5f8b9e7470c5963f
idleberg <git@idleberg.com>
2022-07-08 14:07:10 +0200

    fix readfile

--- index.mjs
+++ index.mjs
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
+import { dirname, join, resolve } from 'node:path';
 import { promises as fs } from 'node:fs';
 import updateNotifier from 'update-notifier';
 import './bin/cli.mjs';
 
 (async () => {
-  const pkg = await fs.readFile('./package.json', 'utf8');
+  const __dirname = resolve(dirname('')); 
+  const pkg = await fs.readFile(join(__dirname, './package.json'), 'utf8');
 
   updateNotifier({
     pkg
